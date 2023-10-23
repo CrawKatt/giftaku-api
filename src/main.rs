27c5841891844fbb09cd0b754daaa2c0b579c453
crawkatt <man849916@gmail.com>
2023-10-23 13:57:18 -0300

    Añadiendo dependencias y primer Response

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,37 @@
 use rocket::tokio::fs::File;
 use rocket::serde::json::Json;
+use rocket::serde::Serialize;
 use std::path::{Path, PathBuf};
 use rocket::{FromForm, get, launch, routes};
+use rocket::fs::FileServer;
 use tokio::io::AsyncReadExt;
 
-#[derive(Debug, PartialEq, FromForm)]
-struct Gif<'r> {
-    name: &'r str,
-    anime: &'r str,
-    url: &'r str,
+#[derive(Serialize)]
+struct GifResponse {
+    action: String,
+    anime: String,
     gif_id: i32,
+    url: String,
+}
+
+/// ruta para obtener un gif
+/// Ejemplo:
+/// `127.0.0.1:8000/api/slap/<anime-aqui>/123`
+#[get("/api/<action>/<anime>/<gif_id>")]
+async fn get_gif(action: String, anime: String, gif_id: i32) -> Option<Json<GifResponse>> {
+    let url = format!("localhost:8000/static/{}.gif", gif_id);
+
+    Some(Json(GifResponse {
+        action,
+        anime,
+        gif_id,
+        url,
+    }))
 }
 
 #[launch]
 fn rocket() -> _ {
-    rocket::build().mount("/", routes![hello, slap, files])
+    rocket::build().mount("/", routes![hello, slap, files, get_gif]).mount("/static", FileServer::from("static"))
 }
 
 #[get("/<file>")]
